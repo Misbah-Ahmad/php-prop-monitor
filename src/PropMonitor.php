dffdc70b5eccea6f6c0c85e94126f975cc6ce142
Misbah Ahmad <imiiucian@gmail.com>
2019-01-21 00:21:51 +0600

    discovered & fix a hole

--- src/PropMonitor.php
+++ src/PropMonitor.php
@@ -2,7 +2,7 @@
 
 namespace PropMonitor;
 
-class PropMonitor{
+class PropMonitor {
     
 
 
@@ -12,10 +12,17 @@ class PropMonitor{
 
         $propCount = count($properties);
 
+
         for($i = 0; $i < $propCount; $i++)
         {
+
             $property = $properties[$i];
             
+            if(is_array($object) && !is_numeric($property))
+            {
+                return false;
+            }
+
             if(is_int($property))
             {
                 //if $property is of interger type, check whether the current object is of array type && looks for the $property key in that array 
@@ -29,12 +36,22 @@ class PropMonitor{
                     return false;
                 }
 
-            } else if(is_numeric($property))
-            {
+            } else if(is_numeric($property) && is_string($property)){
+
                 //$property is a numeric string that contains only numbers
+
+                if(is_object($object) && property_exists($object, $property))
+                {
+                    $object = $object->{$property};
+                    continue;
+                }
+
+
+
                 if(is_object($object))
                 {
                     $object = (array) $object;
+                    $property = (int)$property;
                 }
                 
                 if(!is_array($object))
