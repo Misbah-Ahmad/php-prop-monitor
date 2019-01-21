95b621371b21f4cc99c50a8a7e2114275f3655fb
Misbah Ahmad <imiiucian@gmail.com>
2019-01-21 13:40:14 +0600

    Added get function

--- src/PropMonitor.php
+++ src/PropMonitor.php
@@ -84,7 +84,87 @@ class PropMonitor {
 
     }
    
+    public function get($object, array $properties)
+    {
 
+        $propCount = count($properties);
+
+
+        for($i = 0; $i < $propCount; $i++)
+        {
+
+            $property = $properties[$i];
+            
+            if(is_array($object) && !is_numeric($property))
+            {
+                return false;
+            }
+
+            if(is_int($property))
+            {
+                //if $property is of interger type, check whether the current object is of array type && looks for the $property key in that array 
+                if(is_array($object) && array_key_exists($property, $object))
+                {
+
+                    $object = $object[$property];
+
+                } else {
+
+                    return false;
+                }
+
+            } else if(is_numeric($property) && is_string($property)){
+
+                //$property is a numeric string that contains only numbers
+
+                if(is_object($object) && property_exists($object, $property))
+                {
+                    $object = $object->{$property};
+                    continue;
+                }
+
+
+
+                if(is_object($object))
+                {
+                    $object = (array) $object;
+                    $property = (int)$property;
+                }
+                
+                if(!is_array($object))
+                {
+                    return false;
+                }
+                
+                if(array_key_exists($property, $object)) 
+                {
+                    $object = $object[$property];
+
+                } else {
+                    return false;
+                }
+                
+            } else {
+
+                if(is_object($object) && property_exists($object, $property))
+                {
+                    $object = $object->{$property};
+                } else {
+                    return false;
+                }
+
+            }
+
+        }
+
+        if($object === false || $object === true)
+        {
+            return intval($object);
+        }
+
+        return $object;
+
+    }
 
    
 
